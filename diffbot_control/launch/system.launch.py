acb5ca256da4af0969548b3c1c5a5b08f82b2cc7
truongdang <quangtruonglf@gmail.com>
2025-07-15 00:36:36 +0700

    add myslam and micro ros launch

--- diffbot_control/launch/system.launch.py
+++ diffbot_control/launch/system.launch.py
@@ -5,46 +5,94 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import RegisterEventHandler
+from launch.actions import ExecuteProcess
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 
 from launch_ros.actions import Node
-
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-        package_name = 'diffbot_control' 
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("diffbot_control"), "description", "robot.urdf.xacro"]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
-        gazebo = IncludeLaunchDescription(
-                        PythonLaunchDescriptionSource([os.path.join(
-                                get_package_share_directory(package_name), 'launch', 'gz.launch.py')])
-        )
-        
-        rviz = Node(
-                package='rviz2',
-                executable='rviz2',
-                arguments=['-d', os.path.join(get_package_share_directory(package_name), 'config', 'sim_config.rviz')],
-                output='screen'
-        )
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("diffbot_control"),
+            "config",
+            "ros2_controllers.yaml",
+        ]
+    )
 
-        teleop_keyboard = Node(
-                package='teleop_twist_keyboard',
-                executable='teleop_twist_keyboard',
-                output='screen',
-                prefix = 'xterm -e',
-                parameters=[{'stamped': True}],
-                remappings=[('/cmd_vel', '/diff_controller/cmd_vel')]
-                        
-        )
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        output="both",
+    )
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+        remappings=[
+            ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
+        ],
+    )
 
-        # myslam
-        myslam = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('myslam'),'launch','online_async_launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
-        )
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
 
-        return LaunchDescription([
-                gazebo,
-                rviz,
-                teleop_keyboard,
-                myslam
-        ])
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    # myslam
+    myslam = IncludeLaunchDescription(
+	PythonLaunchDescriptionSource([os.path.join(
+	get_package_share_directory('myslam'),'launch','online_async_launch.py'
+	)]), launch_arguments={'use_sim_time': 'false'}.items()
+    )
+
+    # micro-ros-agent
+    micro_ros = ExecuteProcess(
+            cmd=['micro_ros_agent', 'serial', '--dev', '/dev/ttyACM0'],
+            output='screen'
+    )
+
+    nodes = [
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        myslam,
+        micro_ros
+    ]
+
+    return LaunchDescription(nodes)
+
+
